da7b05851d0d801dc163e36efea7eed7e34fba3f
Mursilaga <mursilaga834@yandex.com>
2018-01-04 23:47:46 +0300

    start work

---
+++ Backup_bro/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Backup_bro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.errLabel = new System.Windows.Forms.Label();
+            this.applyIntervalButton = new System.Windows.Forms.Button();
+            this.numHour = new System.Windows.Forms.NumericUpDown();
+            this.numMin = new System.Windows.Forms.NumericUpDown();
+            this.numSec = new System.Windows.Forms.NumericUpDown();
+            this.hourLabel = new System.Windows.Forms.Label();
+            this.minLabel = new System.Windows.Forms.Label();
+            this.secLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Backup_bro";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = timer_interval;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // errLabel
+            // 
+            this.errLabel.AutoSize = true;
+            this.errLabel.Location = new System.Drawing.Point(12, 161);
+            this.errLabel.Name = "errLabel";
+            this.errLabel.Size = new System.Drawing.Size(35, 13);
+            this.errLabel.TabIndex = 0;
+            this.errLabel.Text = "label1";
+            // 
+            // applyIntervalButton
+            // 
+            this.applyIntervalButton.Location = new System.Drawing.Point(64, 122);
+            this.applyIntervalButton.Name = "applyIntervalButton";
+            this.applyIntervalButton.Size = new System.Drawing.Size(137, 23);
+            this.applyIntervalButton.TabIndex = 1;
+            this.applyIntervalButton.Text = "Применить интервал";
+            this.applyIntervalButton.UseVisualStyleBackColor = true;
+            this.applyIntervalButton.Click += new System.EventHandler(this.applyIntervalButton_Click);
+            // 
+            // numHour
+            // 
+            this.numHour.Location = new System.Drawing.Point(64, 35);
+            this.numHour.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numHour.Name = "numHour";
+            this.numHour.Size = new System.Drawing.Size(42, 20);
+            this.numHour.TabIndex = 2;
+            // 
+            // numMin
+            // 
+            this.numMin.Location = new System.Drawing.Point(64, 61);
+            this.numMin.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numMin.Name = "numMin";
+            this.numMin.Size = new System.Drawing.Size(42, 20);
+            this.numMin.TabIndex = 3;
+            // 
+            // numSec
+            // 
+            this.numSec.Location = new System.Drawing.Point(64, 87);
+            this.numSec.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numSec.Name = "numSec";
+            this.numSec.Size = new System.Drawing.Size(42, 20);
+            this.numSec.TabIndex = 4;
+            // 
+            // hourLabel
+            // 
+            this.hourLabel.AutoSize = true;
+            this.hourLabel.Location = new System.Drawing.Point(112, 37);
+            this.hourLabel.Name = "hourLabel";
+            this.hourLabel.Size = new System.Drawing.Size(35, 13);
+            this.hourLabel.TabIndex = 5;
+            this.hourLabel.Text = "Часы";
+            // 
+            // minLabel
+            // 
+            this.minLabel.AutoSize = true;
+            this.minLabel.Location = new System.Drawing.Point(112, 63);
+            this.minLabel.Name = "minLabel";
+            this.minLabel.Size = new System.Drawing.Size(46, 13);
+            this.minLabel.TabIndex = 6;
+            this.minLabel.Text = "Минуты";
+            // 
+            // secLabel
+            // 
+            this.secLabel.AutoSize = true;
+            this.secLabel.Location = new System.Drawing.Point(112, 89);
+            this.secLabel.Name = "secLabel";
+            this.secLabel.Size = new System.Drawing.Size(51, 13);
+            this.secLabel.TabIndex = 7;
+            this.secLabel.Text = "Секунды";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(43, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(192, 13);
+            this.titleLabel.TabIndex = 8;
+            this.titleLabel.Text = "Интервал создания резевных копий";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.secLabel);
+            this.Controls.Add(this.minLabel);
+            this.Controls.Add(this.hourLabel);
+            this.Controls.Add(this.numSec);
+            this.Controls.Add(this.numMin);
+            this.Controls.Add(this.numHour);
+            this.Controls.Add(this.applyIntervalButton);
+            this.Controls.Add(this.errLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSec)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label errLabel;
+        private System.Windows.Forms.Button applyIntervalButton;
+        private System.Windows.Forms.NumericUpDown numHour;
+        private System.Windows.Forms.NumericUpDown numMin;
+        private System.Windows.Forms.NumericUpDown numSec;
+        private System.Windows.Forms.Label hourLabel;
+        private System.Windows.Forms.Label minLabel;
+        private System.Windows.Forms.Label secLabel;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ Backup_bro/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Backup_bro
+{
+    public partial class Form1 : Form
+    {
+        private Config config;
+        private string conf_path = @"backup_config.txt";
+        private string src_path = "";
+        private string dst_path = "";
+        private string src_exten = "";
+        private Int32 timer_interval = 600000; //10 sec
+        bool IsVisibilityChangeAllowed { get; set; }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (this.IsVisibilityChangeAllowed)
+            {
+                base.SetVisibleCore(value);
+            }
+        }
+        public Form1()
+        {
+            IsVisibilityChangeAllowed = true;
+            bool exist = true;
+            if(File.Exists(conf_path))
+            {
+                string readConf = File.ReadAllText(conf_path, Encoding.Default);
+                config = JsonConvert.DeserializeObject<Config>(readConf);
+                src_path = config.Src;
+                dst_path = config.Dst;
+                timer_interval = Convert.ToInt32(config.Interval, 10);
+                
+            }
+            else
+            {
+                exist = false;
+            }
+            InitializeComponent();
+
+            if(exist)
+            {
+                errLabel.Text = "Файл конфигурации успешно открыт\nSrc: "
+                             + src_path
+                             + "\nDst: "
+                             + dst_path
+                             + "\nInterval: "
+                             + timer_interval / 1000
+                             + " sec\n";
+                
+                if(File.Exists(src_path))
+                {
+                    errLabel.Text += "Файл для создания копий успешно найден";
+                    src_exten = Path.GetExtension(src_path);
+                    IsVisibilityChangeAllowed = false;
+                }
+                else
+                {
+                    errLabel.Text += "Файл для создания копий не найден.\nСоздание копий отключено\n";
+                    timer1.Enabled = false;
+                }
+            }
+            else
+            {
+                errLabel.Text = "Файл конфигурации не найден\nОжидаемый путь: "
+                             + Path.GetFullPath(conf_path)
+                             + "\nПрограмма не инициализирована\nСоздание копий отключено";
+                timer1.Enabled = false;
+            }
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            IsVisibilityChangeAllowed = true;
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+              DateTime date = DateTime.Now;
+              string dst = dst_path + date.ToString("dd.MM.yy") + "//";
+              Directory.CreateDirectory(dst);
+              dst += date.ToString("HH.mm.ss") + src_exten;
+              File.Copy(src_path, dst);
+        }
+
+        private void applyIntervalButton_Click(object sender, EventArgs e)
+        {
+            timer_interval = (Int32)numSec.Value * 1000;
+            timer_interval += (Int32)numMin.Value * 60000;
+            timer_interval += (Int32)numHour.Value * 3600000;
+            config.Interval = timer_interval.ToString();
+            string writeConf = JsonConvert.SerializeObject(config);
+            File.WriteAllText(conf_path, writeConf);
+
+            errLabel.Text = "Новое значение интервала,\nравное "
+                            + timer_interval / 1000
+                            + " сек применено\nи записано в файл конфигурации.";
+        }
+    }
+}
